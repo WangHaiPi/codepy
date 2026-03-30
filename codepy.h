94849409eb6c896466dc31c13f11fad41b2a8a2a
王嗨皮 <82766244@qq.com>
2026-03-30 21:12:27 +0800

    Update codepy.h

--- codepy.h
+++ codepy.h
@@ -1,114 +1,42 @@
-/*
-* codepy.h - Execute Python code in C++
-* 
-* Copyright (c) 2026 WangHaipi
-* 
-* 中文简介：
-* 这是一个头文件库，让你能在 C++ 中直接写 Python 代码。
-* 就像这样：
-*   py << "print('hello')";
-*   py.run();
-* 
-* English Description:
-* A header-only library to write Python code in C++.
-* Usage:
-*   py << "print('hello')";
-*   py.run();
-* 
-* License: MIT
-* https://github.com/WangHaipi/codepy
-*/
-#ifndef CODEPY_H
-#define CODEPY_H
+#include "codepy.h"
+using namespace std;
+using namespace codepy;
 
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <cstdlib>
-#include <ctime>
-#include <codecvt>
-#include <locale>
-#include <windows.h>
-namespace codepy {
-	const std::string MIRROR_TUNA = "https://pypi.tuna.tsinghua.edu.cn/simple";
-	const std::string MIRROR_ALIYUN = "https://mirrors.aliyun.com/pypi/simple";
-	const std::string MIRROR_DOUBAN = "https://pypi.doubanio.com/simple";
-	const std::string MIRROR_USTC = "https://mirrors.ustc.edu.cn/pypi/simple";
-	const std::string MIRROR_TENCENT = "https://mirrors.cloud.tencent.com/pypi/simple";
-	const std::string MIRROR_HUAWEI = "https://mirrors.huaweicloud.com/repository/pypi/simple";
-	const std::string MIRROR_NJU = "https://mirror.nju.edu.cn/pypi/web/simple";
-	const std::string MIRROR_SJTU = "https://mirror.sjtu.edu.cn/pypi/web/simple";
-	const std::string MIRROR_PKU = "https://mirrors.pku.edu.cn/pypi/web/simple";
-	const std::string MIRROR_BFSU = "https://mirrors.bfsu.edu.cn/pypi/web/simple";
-	class Python {
-	private:
-		std::string code_buffer;
-		std::string current_mirror;
-		std::string to_utf8(const std::string& str) {
-#ifdef _WIN32
-			int len = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
-			wchar_t* wstr = new wchar_t[len];
-			MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wstr, len);
-			
-			len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
-			char* ustr = new char[len];
-			WideCharToMultiByte(CP_UTF8, 0, wstr, -1, ustr, len, NULL, NULL);
-			
-			std::string result(ustr);
-			delete[] wstr;
-			delete[] ustr;
-			return result;
-#else
-			return str;
-#endif
-		}
-		
-	public:
-		Python() {
-			std::srand(static_cast<unsigned>(std::time(nullptr)));
-			current_mirror = MIRROR_TUNA;
-		}
-		Python& operator<<(const std::string& py_code) {
-			code_buffer += py_code + "\n";
-			return *this;
-		}
-		void run() {
-			if (code_buffer.empty()) return;
-			std::string filename = "temp_codepy_" + std::to_string(rand()) + ".py";
-			std::ofstream file(filename);
-			file << "# -*- coding: utf-8 -*-\n";
-			file << "import sys\n";
-			file << "import io\n";
-			file << "sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')\n";
-			std::string utf8_code = to_utf8(code_buffer);
-			file << utf8_code;
-			file.close();
-			std::string cmd = "python " + filename;
-			system(cmd.c_str());
-			std::remove(filename.c_str());
-			code_buffer.clear();
-		}
-		void exec(const std::string& line) {
-			*this << line;
-			run();
-		}
-		void pip(const std::string& pack) {
-			std::string cmd = "pip install " + pack;
-			system(cmd.c_str());
-		}
-		void pipun(const std::string& pack) {
-			std::string cmd = "pip uninstall " + pack;
-			system(cmd.c_str());
-		}
-		void set_mirror(const std::string& mirror) {
-			current_mirror = mirror;
-		}
-	};
+int main() {
+	// ========== Basic Execution ==========
+	cout << "=== 1. Basic Python Execution ===" << endl;
+	py << "print('Hello World!')";
+	py << "name = 'WangHaiPi'";
+	py << "print(f'My name is {name}')";
+	py.run();
 	
-} // namespace codepy
-
-static codepy::Python py;
-
-#endif
-
+	// ========== Single Line Execution ==========
+	cout << "\n=== 2. Single Line Execution ===" << endl;
+	py.exec("print('Hello World!')");
+	
+	// ========== Multi-line Code ==========
+	cout << "\n=== 3. Multi-line Code ===" << endl;
+	py << "for i in range(3):";
+	py << "    print(f'The {i}th loop')";
+	py << "print('end')";
+	py.run();
+	
+	// ========== Package Management ==========
+	cout << "\n=== 4. Package Management ===" << endl;
+	py.pipun("requests");
+	py.pip("requests");
+	
+	// ========== List Installed Packages ==========
+	cout << "\n=== 5. List Packages ===" << endl;
+	py.piplist();
+	
+	// ========== Check Python Installation ==========
+	cout << "\n=== 6. Check Python ===" << endl;
+	cout << "Python installed: " << (py.has_python() ? "Yes" : "No") << endl;
+	
+	// ========== Check Package Installation ==========
+	cout << "\n=== 7. Check Package ===" << endl;
+	cout << "requests installed: " << (py.has_package("requests") ? "Yes" : "No") << endl;
+	
+	return 0;
+}
